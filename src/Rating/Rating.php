5939c2e1fd25cd1e663780501f951d7b09fb0347
Alexander S <sysix@sysix-coding.de>
2017-06-23 19:26:10 +0200

    add PSR-2 Standards

--- src/Rating/Rating.php
+++ src/Rating/Rating.php
@@ -7,7 +7,7 @@
  * @copyright Copyright © 2012 - 2014 Michal Chovanec
  * @license Creative Commons Attribution 4.0 International License
  */
- 
+
 namespace Rating;
 
 class Rating
@@ -27,7 +27,7 @@ class Rating
      */
     protected $_ratingA;
     protected $_ratingB;
-    
+
     protected $_scoreA;
     protected $_scoreB;
 
@@ -46,7 +46,7 @@ class Rating
      * @param int $scoreA Score of A
      * @param int $scoreB Score of B
      */
-    public function  __construct($ratingA,$ratingB,$scoreA,$scoreB)
+    public function __construct($ratingA, $ratingB, $scoreA, $scoreB)
     {
         $this->setNewSettings($ratingA, $ratingB, $scoreA, $scoreB);
     }
@@ -60,20 +60,21 @@ class Rating
      * @param int $scoreB Score of B
      * @return self
      */
-    public function setNewSettings($ratingA,$ratingB,$scoreA,$scoreB)
+    public function setNewSettings($ratingA, $ratingB, $scoreA, $scoreB)
     {
-        $this -> _ratingA = $ratingA;
-        $this -> _ratingB = $ratingB;
-        $this -> _scoreA = $scoreA;
-        $this -> _scoreB = $scoreB;
+        $this->_ratingA = $ratingA;
+        $this->_ratingB = $ratingB;
+        $this->_scoreA = $scoreA;
+        $this->_scoreB = $scoreB;
 
-        $expectedScores = $this -> _getExpectedScores($this -> _ratingA,$this -> _ratingB);
-        $this -> _expectedA = $expectedScores['a'];
-        $this -> _expectedB = $expectedScores['b'];
+        $expectedScores = $this->_getExpectedScores($this->_ratingA, $this->_ratingB);
+        $this->_expectedA = $expectedScores['a'];
+        $this->_expectedB = $expectedScores['b'];
 
-        $newRatings = $this ->_getNewRatings($this -> _ratingA, $this -> _ratingB, $this -> _expectedA, $this -> _expectedB, $this -> _scoreA, $this -> _scoreB);
-        $this -> _newRatingA = $newRatings['a'];
-        $this -> _newRatingB = $newRatings['b'];
+        $newRatings = $this->_getNewRatings($this->_ratingA, $this->_ratingB, $this->_expectedA, $this->_expectedB,
+            $this->_scoreA, $this->_scoreB);
+        $this->_newRatingA = $newRatings['a'];
+        $this->_newRatingB = $newRatings['b'];
 
         return $this;
     }
@@ -85,9 +86,9 @@ class Rating
      */
     public function getNewRatings()
     {
-        return array (
-            'a' => $this -> _newRatingA,
-            'b' => $this -> _newRatingB
+        return array(
+            'a' => $this->_newRatingA,
+            'b' => $this->_newRatingB
         );
     }
 
@@ -98,12 +99,12 @@ class Rating
      * @param int $ratingB The Rating of Player B
      * @return array
      */
-    protected function _getExpectedScores($ratingA,$ratingB)
+    protected function _getExpectedScores($ratingA, $ratingB)
     {
-        $expectedScoreA = 1 / ( 1 + ( pow( 10 , ( $ratingB - $ratingA ) / 400 ) ) );
-        $expectedScoreB = 1 / ( 1 + ( pow( 10 , ( $ratingA - $ratingB ) / 400 ) ) );
+        $expectedScoreA = 1 / (1 + (pow(10, ($ratingB - $ratingA) / 400)));
+        $expectedScoreB = 1 / (1 + (pow(10, ($ratingA - $ratingB) / 400)));
 
-        return array (
+        return array(
             'a' => $expectedScoreA,
             'b' => $expectedScoreB
         );
@@ -118,12 +119,12 @@ class Rating
      * @param int $scoreB The score of Player B
      * @return array
      */
-    protected function _getNewRatings($ratingA,$ratingB,$expectedA,$expectedB,$scoreA,$scoreB)
+    protected function _getNewRatings($ratingA, $ratingB, $expectedA, $expectedB, $scoreA, $scoreB)
     {
-        $newRatingA = $ratingA + ( self::KFACTOR * ( $scoreA - $expectedA ) );
-        $newRatingB = $ratingB + ( self::KFACTOR * ( $scoreB - $expectedB ) );
+        $newRatingA = $ratingA + (self::KFACTOR * ($scoreA - $expectedA));
+        $newRatingB = $ratingB + (self::KFACTOR * ($scoreB - $expectedB));
 
-        return array (
+        return array(
             'a' => $newRatingA,
             'b' => $newRatingB
         );
